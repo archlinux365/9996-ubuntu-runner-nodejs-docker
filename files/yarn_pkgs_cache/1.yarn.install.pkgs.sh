3972fbb1f51b002a97c8b086d438f38341e0dda9
gnuhub <gnuhub@gmail.com>
2023-09-01 13:46:29 +0800

    (just fix it)

--- files/yarn_pkgs_cache/1.yarn.install.pkgs.sh
+++ files/yarn_pkgs_cache/1.yarn.install.pkgs.sh
@@ -25,18 +25,23 @@ do
   mkdir -p packages/$line
   cd packages/$line
   git init
-  git checkout master
+  # git checkout master
   git config pull.rebase false
   line2=${line//@}
   git remote add origin git@gitlab.com:graphos/adminos/${line2}.git
   git remote set-url origin git@gitlab.com:graphos/adminos/${line2}.git
   git pull origin master
   rm -rf package-lock.json
+  rm -rf node_modules
+  rm -rf package.json
   yarn init --yes
   yarn add $line #--verbose
   yarn #--verbose
   yarn list > yarn.list.txt
-  p2 "$(date)"
+  find . -name '\.gitignore' -type f -delete
+  git add .
+  git commit -a -m "$(date)"
+  git push origin HEAD
   git remote -v
 } &
   cd $CMD_PATH
